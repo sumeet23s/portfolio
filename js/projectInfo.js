a779cda84f7dec596b3b337f9864822812c5867b
sumeet23s <54115064+sumeet23s@users.noreply.github.com>
2023-04-25 00:36:49 +0530

    Add files via upload
    first push website portfolio

---
+++ js/projectInfo.js
@@ -0,0 +1,58 @@
+const Projects = {
+    Details:{//adding in opposite-chronological order, so it's easy to add later    
+        Template: {
+            ID: "Template",
+            Name: "",
+            SmallDescription: "",
+            LargeDescription: "",
+            DownloadLink: "",
+            EmbedVideoLinkID: "",
+            NoOfProjectImages: 1
+        },
+        ChainReaction: {
+            ID: "ChainReaction",
+            Name: "Chain Reaction",
+            SmallDescription: "Fps Multiplayer Game",
+            LargeDescription: "A fps multiplayer shooter Game made using Unity Engine. By using photon pun 2 as networking engine. The players has to select their particular at the start of the game and has a radar map to show enemies in the radar. It has 4 game modes Death match, Team Death Match, Bounty Match and Capture the flag modes.It has three characters with different rigs and  with different set of animations and uses 7 types of fps weapons.It has two levels currently.Worked on game mechanics, weapons(Guns, grenades) functionalities , level and performance optimisation and photon functionalities. ",
+            EmbedVideoLinkID: "XWXKFm9brFM",
+            NoOfProjectImages: 4
+        },
+        SlingShot: {
+            ID: "SlingShot",
+            Name: "SlingShot",
+            SmallDescription: "Racing Game",
+            LargeDescription: "A racing game using sling as the input for player movement and turning.The player has only has click input.It is a multiplayer game with maximum of 4 players and the goal is to quickly complete the race within the given lap count. Made using unity and photon pun2 as  Network. Worked on Input and Movement synchronisation over the network and in a effective way. Worked on game UI and photon room connections",
+            NoOfProjectImages: 3
+        },
+        ArEducation: {
+            ID: "ArEducation",
+            Name: "ArEduTech",
+            SmallDescription: "Educational AR application",
+            LargeDescription: "AR education Android Application consist of Ar furniture, AR physics , AR chemisty And AR biology for mid school students educational purpose.Worked on physics mechanics and UI. Using unity , Ar kit and  vuforia",
+            NoOfProjectImages: 4
+        },
+        Tankwar: {
+            ID: "Tankwar",
+            Name: "Tank War",
+            SmallDescription: "2D Tank Game",
+            LargeDescription: "It is a 2d tank game for android. Where players can move left or right and need to dodge the bullet, missiles and bombs from the oncoming enemy lines. The enemy lines have 4 levels and ones comes after the another. And Each AI enemy shoots randomly. It is made using unity for android and Cocos creator for webgl Version.",
+            NoOfProjectImages: 4
+        },        
+        NinjaRunner: {
+            ID: "NinjaRunner",
+            Name: "Ninja Runner",
+            SmallDescription: "Endless Runner Game",
+            LargeDescription: "It is endless runner 3d game. In which player has to dodge the obstacles in the given time. It is the first person view game. Made using unity. Worked on random procedural generation of landscapes and obstacles for the player.",
+            NoOfProjectImages: 2
+        },
+
+        SpaceParkour: {
+            ID: "SpaceParkour",
+            Name: "Space Parkour",
+            SmallDescription: "A space parkour Runner Game",
+            LargeDescription: "It is third person 3d Parkour endless runner game. It is made using unity Engine with one character. Made auto generating random level designing. It is made in both android and webGl versions.",
+            NoOfProjectImages: 1
+        },
+   
+    }
+};
